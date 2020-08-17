4f608712121dc6ce2cefd26bce2ca191f9b4ed47
ekelseya <42783392+ekelseya@users.noreply.github.com>
2020-08-17 10:11:45 -0600

    Add About and Contact
    Simple form validation and style updates

--- js/main.js
+++ js/main.js
@@ -24,3 +24,24 @@ if (curHr < 12) {
 } else {
     document.getElementById('greeting').textContent = 'Good Evening! I\'m '
 }
+
+//Contact form validation
+function validateForm() {
+    const name = document.forms['contact-form']['name'].value;
+    const email = document.forms['contact-form']['email'].value;
+    const msg = document.forms['contact-form']['message'].value;
+
+    if (name === ''){
+        document.getElementById('name-error').textContent = 'You must enter a name.';
+        return false;
+    } else if (email == ''){
+        document.getElementById('email-error').textContent = 'You must enter an email.';
+        return false;
+    } else if (msg === ''){
+        document.getElementById('message-error').textContent = 'You must enter a message.';
+        return false;
+    } else {
+        return true;
+    }
+}
+
